828d6a3fd89c71239f7d528895fb164cea5fa1fa
PrasadJambukar <prasadjambukar1@gmail.com>
2024-07-02 10:19:11 +0530

    "error messages displayed"

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,8 @@
-from app import app
+import sys
+import os
 
-application = app
+# Add the path to your Flask application directory
+sys.path.insert(0, "C:/Users/Prasad Jambukar/Desktop/Video-School-CMS")
+
+# Import the Flask application
+from app import app as application

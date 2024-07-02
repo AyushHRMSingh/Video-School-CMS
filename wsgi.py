3062a768bcb1a63cdae70c37a3a67acf3ea27ca3
PrasadJambukar <prasadjambukar1@gmail.com>
2024-07-02 10:20:44 +0530

    "removed wsgi"

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Add the path to your Flask application directory
-sys.path.insert(0, "C:/Users/Prasad Jambukar/Desktop/Video-School-CMS")
-
-# Import the Flask application
-from app import app as application

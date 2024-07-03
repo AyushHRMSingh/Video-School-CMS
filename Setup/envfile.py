66c25e31622aba569fe048db9d33063e2b507455
Heckrexter <102521099+Heckrexter@users.noreply.github.com>
2024-07-03 15:57:12 +0530

    Change folder structure

---
+++ Setup/envfile.py
@@ -0,0 +1,6 @@
+host = 'localhost'
+dbuser = 'root'
+dbpass = 'videopass'
+dbname = 'VIDEOSCHOOL'
+
+# mod_wsgi-express start-server wsgi.py --port=80 --user daemon --group daemon --server-root=/Users/ayushsingh/

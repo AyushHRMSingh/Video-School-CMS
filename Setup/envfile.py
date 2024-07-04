3c7ab49ca9b80da88223ef9d33964d88aa48f197
PrasadJambukar <prasadjambukar1@gmail.com>
2024-07-04 12:33:59 +0530

    "envfile removed"

--- Setup/envfile.py
+++
@@ -1,6 +0,0 @@
-host = 'localhost'
-dbuser = 'root'
-dbpass = 'pass'
-dbname = 'VIDEOSCHOOL'
-
-# mod_wsgi-express start-server wsgi.py --port=80 --user daemon --group daemon --server-root=/Users/ayushsingh/

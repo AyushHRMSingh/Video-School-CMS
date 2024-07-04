15e0336ae90dded446c708dc75d2f36f84c29cdd
Heckrexter <102521099+Heckrexter@users.noreply.github.com>
2024-07-04 12:31:03 +0530

    remove env

--- Setup/envfile.py
+++
@@ -1,6 +0,0 @@
-host = 'localhost'
-dbuser = 'root'
-dbpass = 'videopass'
-dbname = 'VIDEOSCHOOL'
-
-# mod_wsgi-express start-server wsgi.py --port=80 --user daemon --group daemon --server-root=/Users/ayushsingh/
